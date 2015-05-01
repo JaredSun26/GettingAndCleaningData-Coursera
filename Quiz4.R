18cd74f8dd6c7fa4f04221306a0e9640e8ae59ac
Jared <jaredsun26@gmail.com>
2015-04-30 21:47:13 -0700

    add Quiz 4

--- Quiz4.R
+++ Quiz4.R
@@ -1,3 +1,30 @@
+#Q1
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", destfile="survey.csv")
+file.info("survey.csv")
+survey<-read.csv("survey.csv")
+file.remove("survey.csv")
+
+#Q2
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv ","gross.csv")
+gross<-read.csv("gross.csv",header=F,skip=5, nrow=190, 
+                colClasses=c(NA,NA,"NULL",NA,NA,"NULL","NULL","NULL","NULL","NULL"))
+gross$V5<-as.numeric(gsub(",","",gross$V5))
+names(gross)<-c("ShortName","Ranking","Names","GDP")
+mean(gross$GDP)
+
+#Q3
+sum(grepl("^United",gross$Names))
+
+#Q4
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv ","gross.csv")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv","edu.csv")
+
+gross<-read.csv("gross.csv",header=F,skip=5, nrow=190, 
+                colClasses=c(NA,NA,"NULL",NA,NA,"NULL","NULL","NULL","NULL","NULL"))
+gross$V5<-as.numeric(gsub(",","",gross$V5))
+edu<-read.csv("edu.csv")
+length(grep("Fiscal year end:.*June", edu$Special.Notes, ignore.case=TRUE))
+
 
 
 
